0e988aa57fe9c6cf2b17e421e9aefa0cf59f2576
Rupesh Merotha <rupeshmerotha2004@gmail.com>
2025-05-16 10:50:19 +0530

    part1

---
+++ app.js
@@ -0,0 +1,7 @@
+const heading = React.createElement('h1',{},"hello im learning react js"); // return an element 
+// we can make nested create element too
+
+const root = ReactDOM.createRoot(document.getElementById('root'));  
+
+
+root.render(heading);
